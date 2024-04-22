8e33ba169e2c0e30a6bc044ccea3bf8c64d615b3
Tobias Alves <tobiasjose12@gmail.com>
2024-04-22 10:25:46 -0300

    Create Entry.jsx

---
+++ src/components/Entry.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Entry() {
+  return (
+    <div className="intro">
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Entry;  
